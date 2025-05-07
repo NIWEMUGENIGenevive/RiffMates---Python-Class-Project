6a530e6c2b4d870af669c70a46b672c5e4d9da6e
NIWEMUGENIGenevive <genevieveniwemugeni@gmail.com>
2025-05-07 17:37:28 +0800

    done

--- band/admin.py
+++ band/admin.py
@@ -1,3 +1,63 @@
+from typing import Any
 from django.contrib import admin
+from django.utils.html import format_html
+from django.urls import reverse
+from django.db.models.query import QuerySet
+from band.models import Musician,Band
+from datetime import datetime, date
 
-# Register your models here.
+class DecadeListFilter(admin.SimpleListFilter):
+   title= 'decade of birth'
+   parameter_name = 'decade'
+
+   def lookups(self, request, model_admin):
+      result = []
+
+      this_year = datetime.now().year
+      for i in range(1900,this_year, 10):
+          result.append((i, '%s-%s' %(i, i + 9)))
+      return result
+         
+   def queryset(self, request, queryset):
+      start = self.value()
+      if start is None:
+         return queryset
+      start = int(start)
+      return queryset.filter(birth__gte=date(start, 1,1), birth__lte=date(start +9, 12,31))
+
+@admin.register(Musician)
+class MusicianAdmin(admin.ModelAdmin):
+  
+   list_display = ('id','last_name', 'show_weekday','show_bands')
+   search_fields = ('first_name__startswith',)
+   list_filter = (DecadeListFilter,)
+   def show_weekday(self,obj):
+      
+     return obj.birth.strftime('%A')
+
+   show_weekday.short_description = 'weekday of birth'
+   def show_bands(self, obj):
+       band = obj.band_set.all()
+       if len(band) == 0:
+          return format_html('<i> bands</i>')
+       
+       plural  = 's' if len(band) > 1 else ''
+       param = '?id__in= ' + ','.join(str(band.id) for band in band)
+       url = reverse('admin:band_band_changelist') + param
+       return format_html('<a href="{}">{}band{}</a>', url, len(band),plural)
+                          
+   show_bands.short_descrption = 'band'
+
+
+@admin.register(Band)
+class BandAdmin(admin.ModelAdmin):
+   search_fields = ('name__startswith',)
+   class Meta:
+      ordering =('Band', 'venue','Room')
+
+   
+
+                          
+
+
+
